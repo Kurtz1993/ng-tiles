69db7544965b0f2a8dc9bf835ffb65e99b6ab0c3
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-10-03 12:28:47 -0500

    Fix paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgTilesModule } from '@kurtz1993/ng-tiles';
 
 import { AppComponent } from './app.component';
 
