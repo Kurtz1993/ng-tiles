a0c03a74db27b9c25ba5befd8f2e21dd8f62d811
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-10-03 16:09:33 -0500

    Add viewchild to check if customtitle has been passed

--- projects/ngx-cards/src/lib/ngx-card/ngx-card.component.ts
+++ projects/ngx-cards/src/lib/ngx-card/ngx-card.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'ngx-card',
   templateUrl: './ngx-card.component.html',
-  styleUrls: ['./ngx-card.component.scss']
+  styleUrls: ['./ngx-card.component.scss'],
 })
-export class NgxCardComponent implements OnInit {
+export class NgxCardComponent implements OnInit, AfterViewInit {
+  @ViewChild('titleContainer', { static: true })
+  titleContainer: ElementRef<HTMLDivElement>;
 
-  constructor() { }
+  hasCustomTitle = false;
+  // get hasCardTitle() {
+  //   return !!this.cardTitle;
+  // }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    console.log(this.titleContainer, !!this.titleContainer.nativeElement.children.length);
+    // this.hasCustomTitle = !!this.titleContainer.nativeElement.children.length;
   }
-
 }
